6c35bc1217f17d342a7f9e7b560b8bb2333ef3ac
hellomrmoto <jwedmonds22@gmail.com>
2026-04-11 18:35:06 -0700

    Update Episode 6 Thumbnail and URL

--- src/pages/Episodes.tsx
+++ src/pages/Episodes.tsx
@@ -53,9 +53,9 @@ const episodes = [
     id: 6,
     title: `EP-06: I'Ziah Emery Interview`,
     description: `I'Ziah Emery Interview`,
-    duration: '120 Mins',
-    image: 'https://images.unsplash.com/photo-1593697821252-0c9137d9fc45?q=80&w=1000&auto=format&fit=crop',
-    url: 'https://www.youtube.com/@ListeningPleasurePodcast',
+    duration: '1:07:12',
+    image: '/ep6-thumb.png',
+    url: 'https://www.youtube.com/live/eN3EQv2OP0k?si=KStcijrIy1JEmfXk',
     bgPosition: 'center center'
   },
   {
